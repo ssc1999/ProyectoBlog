e7a1303d400be6effbf41d62cbb58f475ba0c9c5
ssc1999 <ssc1099.csmm@gmail.com>
2020-11-25 18:15:37 +0100

    Form updated

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    names = forms.CharField(max_length=100)
-    mail = forms.CharField(max_length=100)
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
+    names = forms.CharField(max_length = 100)
+    number = forms.CharField(max_length = 9)
+    mail = forms.CharField(max_length = 100)
+    message = forms.CharField(widget = forms.Textarea)
